193c62e969011743f6154b93848c10afe9e7fcf9
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-08-11 16:16:34 +1000

    Cairo -> GL

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,8 @@ pushfirst!(LOAD_PATH, joinpath(@__DIR__, "..")) # add Imaginocean to environment
 using
   Documenter,
   Literate,
-  CairoMakie,  # so that Literate.jl does not capture precompilation output or warnings
+  GLMakie,  # so that Literate.jl does not capture precompilation output or warnings
   Imaginocean
-
-CairoMakie.activate!(type = "svg")
 
 #####
 ##### Generate examples
